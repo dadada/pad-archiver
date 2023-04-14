7bc233b72b1b3c74d580ed2d1a8376e5c46362ea
dadada <dadada@dadada.li>
2023-04-14 18:28:27 +0200

    update vendor sha

--- nix/default.nix
+++ nix/default.nix
@@ -3,7 +3,7 @@ buildGoModule {
   pname = "pad-archiver";
   version = "0.0.1";
   src = ../.;
-  vendorSha256 = "sha256-e9I2mhSjLxMuCO9+g13XbDYI15Q879iG1AGZv6otuEA=";
+  vendorSha256 = "sha256-vBni3j3o0P13PJg/Ab1ux9zSVr05Iha/sb8dVTX4G0g=";
   meta = with lib; {
     description = "Archives Etherpads with git";
     homepage = "https://git.fginfo.tu-bs.de/fginfo/pad-archiver";
