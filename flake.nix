86b603f43880fefd4ee3742fdf8b4b4a1cf1c357
dadada <dadada@dadada.li>
2023-05-25 16:52:10 +0200

    flake: activate formatter

--- flake.nix
+++ flake.nix
@@ -5,12 +5,21 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
       rec {
+        formatter = pkgs.nixpkgs-fmt;
+
         packages = flake-utils.lib.flattenTree {
           pad-archiver = pkgs.callPackage ./nix { pkgs = pkgs; };
           gitAndTools = pkgs.gitAndTools;
         };
+
+        checks = {
+          nix-format = pkgs.runCommand "nix-format" { buildInputs = [ formatter ]; } "nixpkgs-fmt --check ${./.} && touch $out";
+        };
+
         defaultPackage = packages.pad-archiver;
         apps.pad-archiver = flake-utils.lib.mkApp { drv = packages.pad-archiver; };
         defaultApp = apps.pad-archiver;
