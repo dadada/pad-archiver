0168a2fb4cc3dfde651be45885e1ddf6a0a50121
Tim Schubert <dadada@dadada.li>
2025-02-03 22:08:09 +0100

    fix: vendorHash

--- nix/default.nix
+++ nix/default.nix
@@ -3,7 +3,7 @@ buildGoModule {
   pname = "pad-archiver";
   version = "0.0.1";
   src = ../.;
-  vendorSha256 = "sha256-vBni3j3o0P13PJg/Ab1ux9zSVr05Iha/sb8dVTX4G0g=";
+  vendorHash = "sha256-vBni3j3o0P13PJg/Ab1ux9zSVr05Iha/sb8dVTX4G0g=";
   ldflags = [ "-s" "-w" ];
   meta = with lib; {
     description = "Archives Etherpads with git";
